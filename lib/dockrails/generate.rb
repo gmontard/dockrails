d5eadba394dd7e5729e3d10d6cfd464b3a66444a
Guillaume Montard <gmontard@users.noreply.github.com>
2017-12-05 11:58:29 +0100

    [FIX] Creating /bundle folder on init

--- lib/dockrails/generate.rb
+++ lib/dockrails/generate.rb
@@ -49,6 +49,8 @@ module Dockrails
 
     def create_folders
       FileUtils.rm_rf('data')
+      FileUtils.rm_rf('bundle')
+      FileUtils.mkdir_p('bundle')
       FileUtils.mkdir_p('data/sql')
       FileUtils.mkdir_p('data/redis') if @config[:redis]
     end
